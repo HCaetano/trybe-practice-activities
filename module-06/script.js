6878a04781e2209cb42b13a2c93dd588efc6204d
Havyner Caetano <hrcgeo@gmail.com>
2020-08-06 15:05:14 -0300

    Fix just validation implementation

--- module-06/script.js
+++ module-06/script.js
@@ -25,33 +25,6 @@ select.addEventListener('change', function () {
   }
 });
 
-
-sendButton.addEventListener('click', function (event) {
-  event.preventDefault();
-  const date = startDate.value;
-  const div = document.createElement('div');
-  let content = 'Name: ' + document.querySelector('#name').value;
-  content = content.concat('\nEmail: ' + document.querySelector('#email').value);
-  content = content.concat('\nCPF: ' + document.querySelector('#cpf').value);
-  content = content.concat('\nAddress: ' + document.querySelector('#address').value);
-  content = content.concat('\nCity: ' + document.querySelector('#city').value);
-  content = content.concat('\nState: ' + state.innerText);
-
-  if (document.querySelectorAll('input[name="residency"]')[0].checked) {
-    residency = document.querySelectorAll('input[name="residency"]')[0].value;
-  } else {
-    residency = document.querySelectorAll('input[name="residency"]')[1].value;
-  }
-
-  content = content.concat('\nResidency type: ' + residency);
-  content = content.concat('\nLast employment summary: ' + document.querySelector('#summary').value);
-  content = content.concat('\nPosition: ' + document.querySelector('#position').value);
-  content = content.concat('\nStart date: ' + date);
-
-  div.innerText = content;
-  body.appendChild(div);
-});
-
 resetButton.addEventListener('click', function () {
   document.querySelector('form').reset();
   body.lastChild.innerText = '';
