1f702773b90171c5612bdbbace920134eaad4528
Havyner Caetano <hrcgeo@gmail.com>
2020-08-11 18:13:45 -0300

    Add the add shift to lesson activity

---
+++ module-07/04-after-class-activities.js
@@ -0,0 +1,27 @@
+const lesson1 = {
+  subject: 'Math',
+  numberOfStudents: 20,
+  professor: 'Maria Clara',
+  shift: 'morning',
+};
+
+const lesson2 = {
+  subject: 'History',
+  numberOfStudents: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  subject: 'Math',
+  numberOfStudents: 10,
+  professor: 'Maria Clara',
+  shift: 'evening',
+};
+
+// 1
+const addShiftToLesson = (lesson, propName, propValue) => {
+  lesson[propName] = propValue;
+}
+
+addShiftToLesson(lesson2, 'shift', 'Morning')
+// console.log(lesson2);
