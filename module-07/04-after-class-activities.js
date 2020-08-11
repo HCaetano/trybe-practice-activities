ce05624c9bcd5a26002ae2d94d53a43752c5c25a
Havyner Caetano <hrcgeo@gmail.com>
2020-08-11 18:23:49 -0300

    Add the create an object from the three lesson objects activity

--- module-07/04-after-class-activities.js
+++ module-07/04-after-class-activities.js
@@ -46,3 +46,8 @@ const listObjectValues = (object) => {
 }
 
 // console.log(listObjectValues(lesson2));
+
+// 5
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+
+// console.log(allLessons);
