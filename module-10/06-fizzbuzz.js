2b672bb09590300f966945dd333416d41055a27e
HCaetano <hrcgeo@gmail.com>
2020-09-04 13:46:29 -0300

    Add testing for 'fizzbuzz' function from 'iamagique' GitHub

---
+++ module-10/06-fizzbuzz.js
@@ -0,0 +1,10 @@
+function fizzBuzz(n) {
+  for (let index = 1; index <= n; index++) {
+    if (index % 3 === 0 && index % 5 === 0) console.log('fizzbuzz');
+    else if (index % 3 === 0) console.log('fizz');
+    else if (index % 5 === 0) console.log('buzz');
+    else console.log(index);
+  }
+}
+
+module.exports = fizzBuzz;
