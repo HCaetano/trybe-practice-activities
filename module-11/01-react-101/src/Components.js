4ec673ae2fc3fe754dd1884962ddd856838ccddb
HCaetano <hrcgeo@gmail.com>
2020-09-10 15:31:42 -0300

    Rename folder, now has number id and is in English

---
+++ module-11/01-react-101/src/Components.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Components extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Havyner</h1>
+        <p>I'm studying to become a full stack developer</p>
+      </div>
+    );
+  }
+}
+
+export default Components;
